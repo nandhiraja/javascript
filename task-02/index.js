695be702f9a806c7367d508ef9a1f3f13487be78
nandhiraja <nandhiraja16@gmail.com>
2026-03-29 22:29:18 +0530

    feat(JS): add expression calcultion funtionality

--- task-02/index.js
+++ task-02/index.js
@@ -1,6 +1,6 @@
 let result = document.getElementById("result-area");
 let input =document.getElementById("input-area");
-result.innerText="Enter values"
+result.innerText="Enter value"
 
 let answer = 0
 let expression = ''
@@ -14,13 +14,15 @@ input.addEventListener("click", e=>{
         if(value in ['1' ,'2' ,'3' ,'4' ,'5' ,'6' ,'7','8','9','0']){
             number+=value;
             expression+=value;
+            result.innerText=expression;
 
         }
         else if(number !=''){   
             expression_list.push(number);
             number=''
             expression+=value;     
-            
+            result.innerText=expression;
+
             expression_list.push(value);
         }
    
@@ -30,21 +32,76 @@ input.addEventListener("click", e=>{
             expression_list.push(number)
             
         }
-        answer=evaluateExpression(expression);
+        answer=evaluateExpression();
         console.log(expression_list)
-
         result.innerText=answer;
+
+
     }
     else if(value =='ac'){
         expression='';
         expression_list=[]
+        result.innerText='';
+
     }
 
-     result.innerText=expression;
     console.log(expression_list)
 })
 
-function evaluateExpression(expression){
-    return 10;
+function evaluateExpression(){
+    // console.log(expression.indexOf('/'))
+    //     console.log(expression.indexOf('-'))
+
+    //         console.log(expression.indexOf('+'))
+
+    //             console.log(expression.indexOf('*'))
+
+    let result = 0;
+
+     while(expression!=[]){
+        console.log(++result , " while ", expression_list)
+        if(expression_list.indexOf('/')!= -1){
+            let indx = expression_list.indexOf('/');
+            let prev = Number(expression_list[indx-1])
+            let next = Number(expression_list[indx+1])
+            let value = prev/next;
+            expression_list.splice(indx-1,3, value);           
+            console.log(expression_list)
+        }
+
+         else if(expression_list.indexOf('*')!= -1){
+            let indx = expression_list.indexOf('*');
+            let prev = Number(expression_list[indx-1])
+            let next = Number(expression_list[indx+1])
+            let value = prev*next;
+            expression_list.splice(indx-1,3, value);           
+            console.log(expression_list)
+        }
+         else if(expression_list.indexOf('+')!= -1){
+            let indx = expression_list.indexOf('+');
+            let prev = Number(expression_list[indx-1])
+            let next = Number(expression_list[indx+1])
+            let value = prev/next;
+            expression_list.splice(indx-1,3, value);           
+            console.log(expression_list)
+        }
+         else if(expression_list.indexOf('-')!= -1){
+            let indx = expression_list.indexOf('-');
+            let prev = Number(expression_list[indx-1])
+            let next = Number(expression_list[indx+1])
+            let value = prev/next;
+            expression_list.splice(indx-1,3, value);           
+            console.log(expression_list)
+        }
+        else {
+            console.log("ENTER EXIT ")
+            // result.innerText= expression_list[0];
+            return expression_list[0]
+        }
+        
+    }
+
+    return result;
+
 }
 
