608406c9e9f00bd4ba499a3ca8b544b51b041920
nandhiraja <nandhiraja16@gmail.com>
2026-03-30 16:48:39 +0530

    feat(JS): Add preProcesser to get necessary data from Json and update in UI

--- task-04/index.js
+++ task-04/index.js
@@ -7,12 +7,65 @@ sendBtn.addEventListener("click", ()=>{
 
     let userInput=input.value
     console.log(userInput)
-    getWeather(userInput)
+    let data = getWeather(userInput);
+   
 
 })
 
-async function getWeather(city){
-    let data = await fetch(`https://api.weatherapi.com/v1/current.json?key=${API_KEY}&q=${city}&aqi=no`);
-    data =  await data.json()
-    console.log(data);
+let city = document.getElementById("city");
+let state = document.getElementById("state");
+let country = document.getElementById("country");
+let localTime = document.getElementById("local-time");
+
+let condition = document.getElementById("condition");
+let humidity = document.getElementById("humidity");
+let temperature = document.getElementById("temperature");
+let pressure = document.getElementById("pressure");
+let windSpeed = document.getElementById("wind-speed");
+
+
+
+
+async function getWeather(user_city){
+
+    let api_data = await fetch(`https://api.weatherapi.com/v1/current.json?key=${API_KEY}&q=${user_city}&aqi=no`);
+    api_data =  await api_data.json()
+    console.log(api_data);
+    let data = preprocessData(api_data);
+
+    console.log("Got preprocess data ," , data)
+
+    city.innerText=data.city;
+    state.innerText = data.state;
+    country.innerText = data.country;
+    localTime.innerText = data.localTime;
+
+    condition.innerText = `${data.condition}`;
+    humidity.innerText = `${data.humidity} %`;
+    temperature.innerText = `${data.temperature} °C`;
+    pressure.innerText = `${data.pressure} hPc`;
+    windSpeed.innerText = `${data.wind} km/h`
+  
+}
+
+function preprocessData(data){
+    console.log("called preprocessData")
+    let current = data.current;
+    let location =data.location;
+    console.log("current data : ",current );
+    console.log("location : ",location)
+
+    data ={
+        'city': location.name,
+        'state': location.region,
+        'country': location.country,
+        'localTime':location.localtime,
+        'condition': current.condition.text,
+        'humidity' : current.humidity,
+        'pressure' : current.pressure_in,
+        'temperature' : current.temp_c,
+        "wind": current.wind_kph
+    }
+    console.log('final_data :' , data)
+    return data
 }
