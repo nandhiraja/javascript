6adb585684f0c47eaa627753f511725286e0243f
nandhiraja <nandhiraja16@gmail.com>
2026-03-30 14:53:40 +0530

    feat(JS): Get real-time weather of a city through weather API

--- task-04/index.js
+++ task-04/index.js
@@ -1,4 +1,4 @@
-
+import { API_KEY } from "./env.js";
 
 let sendBtn = document.getElementById("send");
 let input =  document.getElementById("input")
@@ -7,5 +7,12 @@ sendBtn.addEventListener("click", ()=>{
 
     let userInput=input.value
     console.log(userInput)
-    
+    getWeather(userInput)
+
 })
+
+async function getWeather(city){
+    let data = await fetch(`https://api.weatherapi.com/v1/current.json?key=${API_KEY}&q=${city}&aqi=no`);
+    data =  await data.json()
+    console.log(data);
+}
