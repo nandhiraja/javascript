b624fbf4866be6fc6efeb79980841d42241760a8
nandhiraja <nandhiraja16@gmail.com>
2026-03-30 12:09:16 +0530

    feat(JS): Add dynamic card to show full image of user clicked thumb

--- task-03/index.js
+++ task-03/index.js
@@ -1,13 +1,19 @@
 
 let imageContainer = document.getElementById("container")
 
-
+let cardImage = document.getElementById("card-image")  
+let modelArea =document.getElementById("model-area")
 imageContainer.addEventListener("click" ,e=>{
     console.log(e);
     let clickedClass = e.target.classList
     console.log(clickedClass);
     if(clickedClass=='thumb-img'){
-        console.log(e.target.id)    //  get the clicked image
-        
+        let id = e.target.id;
+        console.log(id)    //  get the clicked image
+        cardImage.innerHTML=`<img src="./images/${id}.jpg" alt="${id} image">`
+        modelArea.style.display="block";
+
     }
 })
+
+
