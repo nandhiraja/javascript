4766b59366593eef018ab5f7df9b5f729e29aeb8
nandhiraja <nandhiraja16@gmail.com>
2026-03-30 13:48:38 +0530

    feat(JS): Add funtionality to get city input form user

---
+++ task-04/index.js
@@ -0,0 +1,11 @@
+
+
+let sendBtn = document.getElementById("send");
+let input =  document.getElementById("input")
+
+sendBtn.addEventListener("click", ()=>{
+
+    let userInput=input.value
+    console.log(userInput)
+    
+})
