6deb8c0a4295aee93d62511df75b76c0c84906bd
nandhiraja <nandhiraja16@gmail.com>
2026-04-02 15:36:52 +0530

    feat(JS): Add functionality to check correct option

--- task-05/index.js
+++ task-05/index.js
@@ -3,6 +3,12 @@ let question = document.getElementById('question')
 
 let buttons = document.querySelectorAll('input')
 
+let currentQuestion = {
+    'question': 'Which is scriping language?',
+    'options' : ['HTML' , 'JAVA','GO','Perl'],
+    'answer' : '1'
+     
+}
 
 
 quizArea.addEventListener('click', e=>{
@@ -15,4 +21,20 @@ quizArea.addEventListener('click', e=>{
 
 })
 
+
+
+function checkAnswer(id){
+    console.log("validation")
+    if(currentQuestion.answer ==id){
+        console.log("clickans is correct")
+       document.getElementById(`opt-${id}`).classList.add('correct')
+    }
+    else{
+        console.log("not correct")
+        document.getElementById(`opt-${currentQuestion.answer}`).classList.add('correct')
+        document.getElementById(`opt-${id}`).classList.add('incorrect')
+
+    }
+    console.log(buttons)
+}
 
