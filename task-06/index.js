9ed445adadbd7d2646e2784157795ecae087e136
nandhiraja <nandhiraja16@gmail.com>
2026-04-04 11:43:25 +0530

    feat(JS): Add function to get coordinate of dragging items

--- task-06/index.js
+++ task-06/index.js
@@ -1,13 +1,26 @@
 let dragables = document.querySelectorAll('.dragable')
-
+let container = document.querySelector('.container')
 
 dragables.forEach(list=>{
     list.addEventListener('dragstart' , (e)=>{
         console.log(e.target.innerText,'is dragging')
         list.classList.add('dragging')
+        
 
     })
-    list.addEventListener('dragover',()=>{
+    list.addEventListener('dragend',()=>{
         list.classList.remove('dragging')
     })
 }) 
+
+container.addEventListener('dragover', (e)=>{
+    e.preventDefault()
+
+    let dragableItems =  [...document.querySelectorAll('.dragable:not(.dragging)')]
+
+    dragableItems.reduce((near,child)=>{
+        let box = child.getBoundingClientRect()
+        console.log(box)
+        })
+
+})
