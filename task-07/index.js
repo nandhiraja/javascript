42fe5a88673d725e3b970e86253cd6e41f6e176e
nandhiraja <nandhiraja16@gmail.com>
2026-04-04 15:34:10 +0530

    feat(JS): Auto reply funtonality using setTimeOut for few seconds

--- task-07/index.js
+++ task-07/index.js
@@ -2,9 +2,13 @@ let userInput = document.getElementById('user-input');
 let sendButton =  document.getElementById("send-btn");
 
 let chatArea = document.getElementById('chat-area');
-
+let respondMessage = ['Shall we meet tomorrow?', 'Where?' , 'Ok fine share the location.','Timing?' ,'Ok we will meet there Byee']
+let messageIndex =0;
 
 sendButton.addEventListener('click',()=>{
+    if(userInput.value=='') {   // no input skip that
+        return
+    }
     console.log(userInput.value)
     let userMessage = document.createElement('div');
     userMessage.classList.add('sender')
@@ -12,6 +16,9 @@ sendButton.addEventListener('click',()=>{
     userMessage.innerHTML=` ${userInput.value}
                 <div class="sender-time timestamp">${currentTime()}</div>`
     chatArea.appendChild(userMessage)
+    setTimeout(sendResponse,2000)    // replay after 2 sec
+    userInput.value=''
+    userInput.focus()
 })
 
 function currentTime(){
@@ -24,4 +31,16 @@ function currentTime(){
     console.log('time : ',time)
     return time[1]+" "+time[2]
 
+}
+
+function sendResponse(){
+    
+    let sendMessage = document.createElement('div');
+    sendMessage.classList.add('receiver')
+    sendMessage.classList.add('message')
+    sendMessage.innerHTML=` ${respondMessage[messageIndex]}
+                <div class="receiver-time timestamp">${currentTime()}</div>`
+    chatArea.appendChild(sendMessage)
+    messageIndex++;
+    
 }
