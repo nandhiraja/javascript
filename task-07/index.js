3143845875e85efb183ee75a826afff3c8952349
nandhiraja <nandhiraja16@gmail.com>
2026-04-04 14:46:21 +0530

    feat: Get user messsage form chat-area

---
+++ task-07/index.js
@@ -0,0 +1,6 @@
+let userInput = document.getElementById('user-input');
+let sendButton =  document.getElementById("send-btn");
+
+sendButton.addEventListener('click',()=>{
+    console.log(userInput.value)
+})
