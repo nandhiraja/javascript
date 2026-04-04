c16a14a64433af8a0e4cb3886e3479bebe6be2bf
nandhiraja <nandhiraja16@gmail.com>
2026-04-04 11:55:36 +0530

    feat(JS): Add function to get nearest element to append the dragged item

--- task-06/index.js
+++ task-06/index.js
@@ -15,12 +15,27 @@ dragables.forEach(list=>{
 
 container.addEventListener('dragover', (e)=>{
     e.preventDefault()
+    let insertElement = getElemetAfterDragging(e.clientY)
+    console.log(insertElement)
 
-    let dragableItems =  [...document.querySelectorAll('.dragable:not(.dragging)')]
-
-    dragableItems.reduce((near,child)=>{
-        let box = child.getBoundingClientRect()
-        console.log(box)
-        })
-
+   
 })
+
+function getElemetAfterDragging(y ){
+     let dragableItems =  [...document.querySelectorAll('.dragable:not(.dragging)')]
+
+    return dragableItems.reduce((near,child)=>{
+        let box = child.getBoundingClientRect()
+        let offset = y-box.top-box.height/2
+        if(offset<0 && offset >near.offset){
+            return {offset: offset,element:child}
+        }
+        else{
+            return near
+        }
+        },
+    {
+        offset:Number.NEGATIVE_INFINITY
+    }).element
+
+}
