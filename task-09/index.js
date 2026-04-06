f8e4fad0990efb2dc447aaf08ab914735612b38e
nandhiraja <nandhiraja16@gmail.com>
2026-04-06 11:41:57 +0530

    feat(js): Add image load funtionalaity after scroll reach botttom

--- task-09/index.js
+++ task-09/index.js
@@ -2,4 +2,18 @@ let container  =  document.getElementById('container')
 
 window.addEventListener('scroll',()=>{
     console.log(window.scrollY)
+    let canLoad = (window.innerHeight+window.scrollY) >= document.documentElement.scrollHeight
+    if(canLoad){
+        loadImages(10)
+    }
 })
+
+
+function loadImages(max){
+    let count = 0;
+    while(count <=max){
+        
+        count++;
+    }
+}
+
